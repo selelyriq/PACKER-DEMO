020ab78481e9810c5a4c53f569d45ddcb2904251
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-28 11:22:58 -0400

    remove

--- packer/scripts/install_aws_agents.sh
+++ packer/scripts/install_aws_agents.sh
@@ -11,9 +11,5 @@ echo "Downloading and installing AWS CloudWatch Agent..."
 curl -L -o /tmp/amazon-cloudwatch-agent.rpm https://s3.us-east-2.amazonaws.com/amazoncloudwatch-agent-us-east-2/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 sudo yum install -y /tmp/amazon-cloudwatch-agent.rpm
 sudo systemctl enable amazon-cloudwatch-agent
-sudo systemctl start amazon-cloudwatch-agent
 
 echo "AWS Agents installed successfully."
-
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:/cloudwatch-agent/config -s
-echo "CloudWatch Agent fetched config from SSM and started."
